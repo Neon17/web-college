3fc4393b3630ae0f7df3098b92d8fc28e657301a
Neon17 <neon.neupane.9@gmail.com>
2025-05-17 10:34:28 +0545

    Calculator UI made

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,26 @@ app.get('/', (req,res)=>{
     })
 })
 
+const quotes = [
+    {'id': 1, 'quote': 'The greatest glory in living lies not in never falling, but in rising every time we fall.'},
+    {'id': 2, 'quote': 'The way to get started is to quit talking and begin doing.'},
+    {'id': 3, 'quote': 'Life is what happens when you\'re busy making other plans.'},
+    {'id': 4, 'quote': 'Get busy living or get busy dying.'},
+    {'id': 5, 'quote': 'You have within you right now, everything you need to deal with whatever the world can throw at you.'}
+];
+
+app.get('/quotes', (req,res)=>{
+    res.json(quotes);
+})
+
+app.post('/quotes', (req,res)=>{
+    const newQuote = {
+        id: quotes.length + 1,
+        quote: req.body.quote
+    }
+    quotes.push(newQuote)
+    res.status(201).json(quotes)
+})
+
 
 app.listen(3030, ()=>console.log("Server listening on http://localhost:3030"))
